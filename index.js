d3820eefac63d8b77844c4eab3fee26815905613
Phillip Lanclos <flipxfx@gmail.com>
2018-08-16 15:00:40 -0500

    Add direct url (#23)
    * Add direct url
    * Add package.lock
    * Fix GitLab fixture with UNLICENS

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function download (repo, dest, opts, fn) {
   var clone = opts.clone || false
 
   repo = normalize(repo)
-  var url = getUrl(repo, clone)
+  var url = repo.url || getUrl(repo, clone)
 
   if (clone) {
     gitclone(url, dest, { checkout: repo.checkout, shallow: repo.checkout === 'master' }, function (err) {
@@ -38,11 +38,13 @@ function download (repo, dest, opts, fn) {
       }
     })
   } else {
-    downloadUrl(url, dest, { extract: true, strip: 1, mode: '666', headers: { accept: 'application/zip' } }).then(data => {
-      fn()
-    }).catch(err => {
-      fn(err)
-    })
+    downloadUrl(url, dest, { extract: true, strip: 1, mode: '666', headers: { accept: 'application/zip' } })
+      .then(function (data) {
+        fn()
+      })
+      .catch(function (err) {
+        fn(err)
+      })
   }
 }
 
@@ -54,29 +56,43 @@ function download (repo, dest, opts, fn) {
  */
 
 function normalize (repo) {
-  var regex = /^((github|gitlab|bitbucket):)?((.+):)?([^/]+)\/([^#]+)(#(.+))?$/
+  var regex = /^(?:(direct):([^#]+)(?:#(.+))?)$/
   var match = regex.exec(repo)
-  var type = match[2] || 'github'
-  var origin = match[4] || null
-  var owner = match[5]
-  var name = match[6]
-  var checkout = match[8] || 'master'
 
-  if (origin == null) {
-    if (type === 'github')
-      origin = 'github.com'
-    else if (type === 'gitlab')
-      origin = 'gitlab.com'
-    else if (type === 'bitbucket')
-      origin = 'bitbucket.org'
-  }
+  if (match) {
+    var url = match[2]
+    var checkout = match[3] || 'master'
 
-  return {
-    type: type,
-    origin: origin,
-    owner: owner,
-    name: name,
-    checkout: checkout
+    return {
+      type: 'direct',
+      url: url,
+      checkout: checkout
+    }
+  } else {
+    regex = /^(?:(github|gitlab|bitbucket):)?(?:(.+):)?([^\/]+)\/([^#]+)(?:#(.+))?$/
+    match = regex.exec(repo)
+    var type = match[1] || 'github'
+    var origin = match[2] || null
+    var owner = match[3]
+    var name = match[4]
+    var checkout = match[5] || 'master'
+
+    if (origin == null) {
+      if (type === 'github')
+        origin = 'github.com'
+      else if (type === 'gitlab')
+        origin = 'gitlab.com'
+      else if (type === 'bitbucket')
+        origin = 'bitbucket.com'
+    }
+
+    return {
+      type: type,
+      origin: origin,
+      owner: owner,
+      name: name,
+      checkout: checkout
+    }
   }
 }
 
@@ -125,8 +141,6 @@ function getUrl (repo, clone) {
       url = origin + repo.owner + '/' + repo.name + '/repository/archive.zip?ref=' + repo.checkout
     else if (repo.type === 'bitbucket')
       url = origin + repo.owner + '/' + repo.name + '/get/' + repo.checkout + '.zip'
-    else
-      url = github(repo)
   }
 
   return url
--- test/index.js
+++ test/index.js
@@ -117,4 +117,42 @@ describe('download-git-repo', function () {
   describe('via bitbucket', function () {
     runType('bitbucket')
   })
+
+  describe('via direct', function () {
+    it('downloads master branch', function (done) {
+      download('direct:https://gitlab.com/flipxfx/download-git-repo-fixture/repository/archive.zip', 'test/tmp', function (err) {
+        var actual = read('test/tmp', filter)
+        var expected = read('test/fixtures/gitlab/master')
+        assert.deepEqual(actual, expected)
+        done()
+      })
+    })
+
+    it('downloads a branch', function (done) {
+      download('direct:https://gitlab.com/flipxfx/download-git-repo-fixture/repository/archive.zip?ref=my-branch', 'test/tmp', function (err) {
+        var actual = read('test/tmp', filter)
+        var expected = read('test/fixtures/gitlab/my-branch')
+        assert.deepEqual(actual, expected)
+        done()
+      })
+    })
+
+    it('clones master branch', function (done) {
+      download('direct:https://gitlab.com/flipxfx/download-git-repo-fixture.git', 'test/tmp', { clone: true }, function (err) {
+        var actual = read('test/tmp', filter)
+        var expected = read('test/fixtures/gitlab/master')
+        assert.deepEqual(actual, expected)
+        done()
+      })
+    })
+
+    it('clones a branch', function (done) {
+      download('direct:https://gitlab.com/flipxfx/download-git-repo-fixture.git#my-branch', 'test/tmp', { clone: true }, function (err) {
+        var actual = read('test/tmp', filter)
+        var expected = read('test/fixtures/gitlab/my-branch')
+        assert.deepEqual(actual, expected)
+        done()
+      })
+    })
+  })
 })
