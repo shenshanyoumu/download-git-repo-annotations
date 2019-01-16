e54cd854a3babb62dbb80567ebe162cf14b13475
zong lin. xiang <xianglinzong@xwfintech.net>
2019-01-16 14:40:10 +0800

    feat:almost implement

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ Download and extract a git repository (GitHub, GitLab, Bitbucket) from node.
 Download a git `repository` to a `destination` folder with `options`, and `callback`.
 
 #### repository
+
 The shorthand repository string to download the repository from:
 
 - **GitHub** - `github:owner/name` or simply `owner/name`
@@ -33,70 +34,110 @@ If using `direct` without clone, you must pass the full url to the zip file, inc
 If using `direct` with clone, you must pass the full url to the git repo and you can specify a branch like `direct:url#my-branch`.
 
 #### destination
+
 The file path to download the repository to.
 
 #### options
+
 An optional options object parameter with download options. Options include:
 
 - `clone` - boolean default `false` - If true use `git clone` instead of an http download. While this can be a bit slower, it does allow private repositories to be used if the appropriate SSH keys are setup.
 
 #### callback
+
 The callback function as `function (err)`.
 
 ## Examples
+
 ### Shorthand
+
 Using http download from Github repository at master.
+
 ```javascript
-download('flipxfx/download-git-repo-fixture', 'test/tmp', function (err) {
-  console.log(err ? 'Error' : 'Success')
-})
+download("flipxfx/download-git-repo-fixture", "test/tmp", function(err) {
+  console.log(err ? "Error" : "Success");
+});
 ```
 
 Using git clone from Bitbucket repository at my-branch.
+
 ```javascript
-download('bitbucket:flipxfx/download-git-repo-fixture#my-branch', 'test/tmp', { clone: true }, function (err) {
-  console.log(err ? 'Error' : 'Success')
-})
+download(
+  "bitbucket:flipxfx/download-git-repo-fixture#my-branch",
+  "test/tmp",
+  { clone: true },
+  function(err) {
+    console.log(err ? "Error" : "Success");
+  }
+);
 ```
 
 Using http download from GitLab repository with custom origin.
+
 ```javascript
-download('gitlab:mygitlab.com:flipxfx/download-git-repo-fixture#my-branch', 'test/tmp', function (err) {
-  console.log(err ? 'Error' : 'Success')
-})
+download(
+  "gitlab:mygitlab.com:flipxfx/download-git-repo-fixture#my-branch",
+  "test/tmp",
+  function(err) {
+    console.log(err ? "Error" : "Success");
+  }
+);
 ```
 
 Using git clone from GitLab repository with custom origin and protocol.
 Note that the repository type (`github`, `gitlab` etc.) is not required if cloning from a custom origin.
+
 ```javascript
-download('https://mygitlab.com:flipxfx/download-git-repo-fixture#my-branch', 'test/tmp', { clone: true }, function (err) {
-  console.log(err ? 'Error' : 'Success')
-})
+download(
+  "https://mygitlab.com:flipxfx/download-git-repo-fixture#my-branch",
+  "test/tmp",
+  { clone: true },
+  function(err) {
+    console.log(err ? "Error" : "Success");
+  }
+);
 ```
 
 ### Direct
+
 Using http download from direct url.
+
 ```javascript
-download('direct:https://gitlab.com/flipxfx/download-git-repo-fixture/repository/archive.zip', 'test/tmp', function (err) {
-  console.log(err ? 'Error' : 'Success')
-})
+download(
+  "direct:https://gitlab.com/flipxfx/download-git-repo-fixture/repository/archive.zip",
+  "test/tmp",
+  function(err) {
+    console.log(err ? "Error" : "Success");
+  }
+);
 ```
 
 Using git clone from direct url at master.
+
 ```javascript
-download('direct:https://gitlab.com/flipxfx/download-git-repo-fixture.git', 'test/tmp', { clone: true }, function (err) {
-  console.log(err ? 'Error' : 'Success')
-})
+download(
+  "direct:https://gitlab.com/flipxfx/download-git-repo-fixture.git",
+  "test/tmp",
+  { clone: true },
+  function(err) {
+    console.log(err ? "Error" : "Success");
+  }
+);
 ```
 
 Using git clone from direct url at my-branch.
+
 ```javascript
-download('direct:https://gitlab.com/flipxfx/download-git-repo-fixture.git#my-branch', 'test/tmp', { clone: true }, function (err) {
-  console.log(err ? 'Error' : 'Success')
-})
+download(
+  "direct:https://gitlab.com/flipxfx/download-git-repo-fixture.git#my-branch",
+  "test/tmp",
+  { clone: true },
+  function(err) {
+    console.log(err ? "Error" : "Success");
+  }
+);
 ```
 
 ## License
 
 MIT
-
--- index.js
+++ index.js
@@ -1,89 +1,110 @@
-var downloadUrl = require('download')
-var gitclone = require('git-clone')
-var rm = require('rimraf').sync
+// download模块用于根据指定URL下载资源，而git-clone基于shell命令克隆代码
+var downloadUrl = require("download");
+var gitclone = require("git-clone");
+var rm = require("rimraf").sync;
+
+// 对外暴露download接口
+module.exports = download;
 
 /**
- * Expose `download`.
- */
-
-module.exports = download
-
-/**
- * Download `repo` to `dest` and callback `fn(err)`.
  *
- * @param {String} repo
- * @param {String} dest
- * @param {Object} opts
- * @param {Function} fn
+ * @param {String} repo 下载的代码路径和可选的分支
+ * @param {String} dest 本地目标路径
+ * @param {Object} opts 控制下载行为的配置项
+ * @param {Function} fn 下载完成或失败后的回调
  */
 
-function download (repo, dest, opts, fn) {
-  if (typeof opts === 'function') {
-    fn = opts
-    opts = null
+function download(repo, dest, opts, fn) {
+  if (typeof opts === "function") {
+    fn = opts;
+    opts = null;
   }
-  opts = opts || {}
-  var clone = opts.clone || false
 
-  repo = normalize(repo)
-  var url = repo.url || getUrl(repo, clone)
+  // 默认使用https下载方式，通过在opts配置clone为true可使用ssh的方式克隆代码
+  opts = opts || {};
+  var clone = opts.clone || false;
+
+  // 将传入的repo字符串进行归一化处理
+  repo = normalize(repo);
+
+  // 针对非direct前缀的repo形式，需要转换为repo的URL路径
+  var url = repo.url || getUrl(repo, clone);
 
   if (clone) {
-    gitclone(url, dest, { checkout: repo.checkout, shallow: repo.checkout === 'master' }, function (err) {
-      if (err === undefined) {
-        rm(dest + '/.git')
-        fn()
-      } else {
-        fn(err)
+    gitclone(
+      url,
+      dest,
+      { checkout: repo.checkout, shallow: repo.checkout === "master" },
+      function(err) {
+        if (err === undefined) {
+          rm(dest + "/.git");
+          fn();
+        } else {
+          fn(err);
+        }
       }
-    })
+    );
   } else {
-    downloadUrl(url, dest, { extract: true, strip: 1, mode: '666', headers: { accept: 'application/zip' } })
-      .then(function (data) {
-        fn()
+    // 基于http协议下载ZIP文件
+    downloadUrl(url, dest, {
+      extract: true,
+      strip: 1,
+      mode: "666",
+      headers: { accept: "application/zip" }
+    })
+      .then(function(data) {
+        fn();
       })
-      .catch(function (err) {
-        fn(err)
-      })
+      .catch(function(err) {
+        fn(err);
+      });
   }
 }
 
 /**
- * Normalize a repo string.
+ * 对传递的仓库路径字符串进行归一化
  *
  * @param {String} repo
  * @return {Object}
  */
 
-function normalize (repo) {
-  var regex = /^(?:(direct):([^#]+)(?:#(.+))?)$/
-  var match = regex.exec(repo)
+function normalize(repo) {
+  // 对针对direct类型repo字符串进行解析；同时抽取出仓库地址路径，以及分支字段等
+  var regex = /^(?:(direct):([^#]+)(?:#(.+))?)$/;
+  var match = regex.exec(repo);
 
+  // direct标识的repo字符串，是完全的仓库路径
   if (match) {
-    var url = match[2]
-    var checkout = match[3] || 'master'
+    var url = match[2];
+    var checkout = match[3] || "master";
 
     return {
-      type: 'direct',
+      type: "direct",
       url: url,
       checkout: checkout
-    }
+    };
   } else {
-    regex = /^(?:(github|gitlab|bitbucket):)?(?:(.+):)?([^\/]+)\/([^#]+)(?:#(.+))?$/
-    match = regex.exec(repo)
-    var type = match[1] || 'github'
-    var origin = match[2] || null
-    var owner = match[3]
-    var name = match[4]
-    var checkout = match[5] || 'master'
+    // 基于github|gitlab|bitbucket前缀的repo字符串解析
+    regex = /^(?:(github|gitlab|bitbucket):)?(?:(.+):)?([^\/]+)\/([^#]+)(?:#(.+))?$/;
+    match = regex.exec(repo);
+
+    // 默认为github仓库类型
+    var type = match[1] || "github";
+
+    // 这个源表示开发者自定义的仓库源，比如内部基于gitlab搭建的服务
+    var origin = match[2] || null;
+
+    // 这三种仓库类型对外提供的项目路径都会包括owner和name信息
+    var owner = match[3];
+    var name = match[4];
+
+    // 默认分支为master
+    var checkout = match[5] || "master";
 
     if (origin == null) {
-      if (type === 'github')
-        origin = 'github.com'
-      else if (type === 'gitlab')
-        origin = 'gitlab.com'
-      else if (type === 'bitbucket')
-        origin = 'bitbucket.com'
+      if (type === "github") origin = "github.com";
+      else if (type === "gitlab") origin = "gitlab.com";
+      else if (type === "bitbucket") origin = "bitbucket.com";
     }
 
     return {
@@ -92,7 +113,7 @@ function normalize (repo) {
       owner: owner,
       name: name,
       checkout: checkout
-    }
+    };
   }
 }
 
@@ -103,15 +124,15 @@ function normalize (repo) {
  * @return {String}
  */
 
-function addProtocol (origin, clone) {
+function addProtocol(origin, clone) {
+  // 当采用github/gitlab/bucket等前缀的repo形式时，根据传入的opts参数是否包括clone，调用不同的下载形式
+  // 以git@开头形式基于ssh的克隆；而采用https形式使用ZIP下载
   if (!/^(f|ht)tps?:\/\//i.test(origin)) {
-    if (clone)
-      origin = 'git@' + origin
-    else
-      origin = 'https://' + origin
+    if (clone) origin = "git@" + origin;
+    else origin = "https://" + origin;
   }
 
-  return origin
+  return origin;
 }
 
 /**
@@ -121,27 +142,51 @@ function addProtocol (origin, clone) {
  * @return {String}
  */
 
-function getUrl (repo, clone) {
-  var url
+function getUrl(repo, clone) {
+  var url;
 
-  // Get origin with protocol and add trailing slash or colon (for ssh)
-  var origin = addProtocol(repo.origin, clone)
-  if (/^git\@/i.test(origin))
-    origin = origin + ':'
-  else
-    origin = origin + '/'
+  //为repo的仓库源添加下载协议
+  var origin = addProtocol(repo.origin, clone);
 
-  // Build url
-  if (clone) {
-    url = origin + repo.owner + '/' + repo.name + '.git'
+  // 如果是git@开头的协议，则添加":"来拼接origin和owner
+  if (/^git\@/i.test(origin)) {
+    origin = origin + ":";
   } else {
-    if (repo.type === 'github')
-      url = origin + repo.owner + '/' + repo.name + '/archive/' + repo.checkout + '.zip'
-    else if (repo.type === 'gitlab')
-      url = origin + repo.owner + '/' + repo.name + '/repository/archive.zip?ref=' + repo.checkout
-    else if (repo.type === 'bitbucket')
-      url = origin + repo.owner + '/' + repo.name + '/get/' + repo.checkout + '.zip'
+    origin = origin + "/";
   }
 
-  return url
+  // 如果download函数的opts参数设置了clone属性
+  if (clone) {
+    url = origin + repo.owner + "/" + repo.name + ".git";
+  } else {
+    // 基于github仓库类别，且没有设置clone属性，则使用zip下载并解析
+    if (repo.type === "github")
+      url =
+        origin +
+        repo.owner +
+        "/" +
+        repo.name +
+        "/archive/" +
+        repo.checkout +
+        ".zip";
+    else if (repo.type === "gitlab")
+      url =
+        origin +
+        repo.owner +
+        "/" +
+        repo.name +
+        "/repository/archive.zip?ref=" +
+        repo.checkout;
+    else if (repo.type === "bitbucket")
+      url =
+        origin +
+        repo.owner +
+        "/" +
+        repo.name +
+        "/get/" +
+        repo.checkout +
+        ".zip";
+  }
+
+  return url;
 }
